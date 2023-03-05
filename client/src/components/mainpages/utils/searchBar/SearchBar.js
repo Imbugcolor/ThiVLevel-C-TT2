8d13e584a23929810f2386eed58af004016cfada
Imbugcolor <viethd123456@gmail.com>
2023-03-05 11:43:32 +0700

    Search bar p2

--- client/src/components/mainpages/utils/searchBar/SearchBar.js
+++ client/src/components/mainpages/utils/searchBar/SearchBar.js
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { GlobalState } from '../../../../GlobalState'
+import { GoSearch } from 'react-icons/go'
 
 function SearchBar() {
 
@@ -38,31 +40,44 @@ function SearchBar() {
         <div className="filter_menu product">
 
             <div className="search search__bar_wrapper" >
+                <div className="search__input_icon">
+                    <GoSearch />
+                </div>
                 <input className="search-input-bd-none search__bar_input" type="text" placeholder="Nhập sản phẩm bạn muốn tìm kiếm ..."
                     value={wordEntered}
                     onKeyPress={(e) => {handleSearch(e,wordEntered)}}
                     onChange={handleSuggest}
                     onFocus={() => setOpen(true)}
                     onBlur={e => {
-                        e.relatedTarget?.classList.contains('result-link') ?
+                        e.relatedTarget?.classList.contains('redirect_item_result') ?
                             e.preventDefault() :
                             setOpen(false)
                     }}
                 />
-          
                 {
                     open && filterData.length > 0 ?
-                        <ul className="result-list">
+                        <ul className="list_item_suggest">
                             {
                                 filterData.map((item, index) => {
-                                    return index > 4 ? null :
-                                        <li key={item._id} className="result-item">
-                                            <a href="#!" className="result-link" onClick={(e) => {
-                                                setWordEntered(e.target.innerText)                                            
-                                                setOpen(false)
-                                            }}>
-                                                {item.title}
-                                            </a>
+                                    return index > 15 ? null :
+                                        <li key={item._id} className="item_suggest_result">
+                                            <div className='left__item_suggest_result'>
+                                                <Link to={`/detail/${item._id}`} className="redirect_item_result" onClick={(e) => {
+                                                    setWordEntered(e.target.innerText)                                            
+                                                    setOpen(false)
+                                                }}>
+                                                    {item.title}
+                                                </Link>
+                                                <span>{item.price} $</span>
+                                            </div>
+                                            <div className='right__item_suggest_result'>
+                                                <Link to={`/detail/${item._id}`} className="redirect_item_result" onClick={(e) => {
+                                                    setWordEntered(e.target.innerText)                                            
+                                                    setOpen(false)
+                                                }}>
+                                                    <img src={item.images[0].url}/>
+                                                </Link>    
+                                            </div>                                          
                                         </li>
                                 })
                             }
