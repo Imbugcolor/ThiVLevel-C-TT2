52f758691c00fde9d6e7636fa18cb23bd2e6b960
Imbugcolor <viethd123456@gmail.com>
2023-03-16 21:35:22 +0700

    upgrade order details page UI

--- client/src/components/mainpages/history/OrderDetails.js
+++ client/src/components/mainpages/history/OrderDetails.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { GlobalState } from '../../../GlobalState'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faTruck, faMapLocation } from '@fortawesome/free-solid-svg-icons'
 import { toast } from 'react-toastify'
 import axios from 'axios'
 import moment from 'moment'
 import Loading from '../utils/loading/Loading'
+import CodLogo from '../../../images/cod-logo.webp'
+import VisaLogo from '../../../images/visa-logo.png'
+import { IoTrashBin } from 'react-icons/io5'
 
 function OrderDetails() {
     const state = useContext(GlobalState)
@@ -60,52 +61,15 @@ function OrderDetails() {
         loading ? <div><Loading /></div> :
         <div className="history-page res-row">
             <div className="order-infor-container col l-12 m-12 c-12">
-                <div className="res-row">
-                    <div className="order-infor col l-4 m-4 c-12">
-                        <div className="thumbnail">
-                            <span>
-                                <FontAwesomeIcon icon={faUser} />
-                            </span>
-                        </div>
-                        <div className="infor name">
-                            <span>Người nhận: {orderDetails.address.recipient_name || orderDetails.name}</span>
-                            <span>Email: {orderDetails.email}</span>
-                            <span>Số điện thoại: +84 {orderDetails.phone}</span>
-                        </div>
-                    </div>
-                    <div className="order-infor col l-4 m-4 c-12">
-                        <div className="thumbnail">
-                            <span>
-                                <FontAwesomeIcon icon={faTruck} />
-                            </span>
-                        </div>
-                        <div className="infor status">
-                            <span>Trạng thái: {orderDetails.status}</span>
-                            <span>Phương thức thanh toán: {orderDetails.method}</span>
-                        </div>
-                    </div>
-                    <div className="order-infor col l-4 m-4 c-12">
-                        <div className="thumbnail">
-                            <span>
-                                <FontAwesomeIcon icon={faMapLocation} />
-                            </span>
-                        </div>
-                        <div className="infor address">
-                            <span>
-                                Địa chỉ nhận hàng:
-                                {
-                                    orderDetails.method === 'Paypal' ? ` ${orderDetails.address.address_line_1}, ${orderDetails.address.admin_area_2}` :
-                                        ` ${(orderDetails.address.detailAddress || '')} ${orderDetails.address.ward.label}, ${orderDetails.address.district.label}, ${orderDetails.address.city.label}`
-                                }
-                            </span>
-                        </div>
-                    </div>
+                <div className='order__infor_heading'>
+                    <h3>Chi tiết đơn hàng</h3>
+                    <p>Ngày đặt: {new Date(orderDetails.createdAt).toLocaleDateString() + ' ' + moment(orderDetails.createdAt).format('LT')}</p>
                 </div>
             </div>
 
             <div className="order-detail col l-12 m-12 c-12">
                 <div className="res-row">
-                    <div className='list-product-order-client col l-9 m-12 c-12'>
+                    <div className='list-product-order-client col l-8 m-12 c-12'>
                         <table className="oder-product-list-table">
                             <thead className="table-header">
                                 <tr>
@@ -145,68 +109,122 @@ function OrderDetails() {
                             </tbody>
                         </table>
                     </div>
-                    <div className='pay-infor-wrapper col l-3 m-12 c-12'>
-                        <div className="pay-infor">
-
-                            <div className="item fw600">
-                                <div>Mã đơn</div>
-                                <div style={{ textTransform: 'uppercase', wordBreak: 'break-word' }}>{orderDetails._id}</div>
-                            </div>
-                            <div className="item fw600">
+                    <div className='pay-infor-wrapper col l-4 m-12 c-12'>
+                        <div className="heading__pay-infor">
+                            <div>Mã đơn</div>
+                            <div style={{ textTransform: 'uppercase', wordBreak: 'break-word', color: '#555' }}>{'#'+ orderDetails._id}</div>
+                        </div>
+                        <div className="pay-infor">                              
+                            <div className="item">
                                 <div>Ngày đặt</div>
-                                <div>{new Date(orderDetails.createdAt).toLocaleDateString()} {moment(orderDetails.createdAt).format('LT')}</div>
+                                <div style={{ textTransform: 'uppercase', wordBreak: 'break-word', color: '#555' }}>{new Date(orderDetails.createdAt).toLocaleDateString()} {moment(orderDetails.createdAt).format('LT')}</div>
                             </div>
                             <div className="divider"></div>
                             <div className="item">
                                 <div>Tổng cộng</div>
-                                <div>${orderDetails.total}</div>
+                                <div>${orderDetails.total.toFixed(2)}</div>
                             </div>
                             <div className="item">
                                 <div>Phí vận chuyển</div>
-                                <div>$0</div>
+                                <div>$0.00</div>
                             </div>
                             <div className="item">
                                 <div>Phương thức thanh toán</div>
                                 <div>{orderDetails.method}</div>
                             </div>
-                            {
-                                orderDetails.method === 'Paypal' ?
-                                    <div className="item">
-                                        <div>Mã thanh toán</div>
-                                        <div>{orderDetails.paymentID}</div>
-                                    </div>
-                                    : ''
-                            }
                             <div className="divider"></div>
                             <div className="item fw600">
                                 <div>Tổng thanh toán</div>
-                                <div style={{ color: '#d93938' }}>${orderDetails.total}</div>
-                            </div>
-                            <div className="item fw600">
-                                <div>Thời gian thanh toán</div>
-                                <div>
-                                    {orderDetails.isPaid === true ?
-                                        `Đã thanh toán vào ${new Date(orderDetails.updatedAt).toLocaleDateString() + ' ' + moment(orderDetails.updatedAt).format('LT')}`
-                                        :  'Chưa thanh toán'
-                                    }
-                                </div>
+                                <div style={{ color: '#d93938' }}>${orderDetails.total.toFixed(2)}</div>
+                            </div>                    
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className='order__bottom_info res-row'>
+                    <div className='customer__order_infor box-style_infor col l-3 m-6 c-12'>
+                        <span className='heading__box'>Thông tin khách hàng</span>
+                        <div className='box-detail_infor'>
+                            <p>Tên: {orderDetails.address.recipient_name || orderDetails.name}</p>
+                            <p>Email: {orderDetails.email}</p>
+                            <p>Số điện thoại: +84 {orderDetails.phone}</p>
+                        </div>
+                    </div>
+                    <div className='shipping__order_infor box-style_infor col l-3 m-6 c-12'>
+                        <span className='heading__box'>Thông tin giao hàng</span>
+                        <div className='box-detail_infor'>
+                            <span>Địa chỉ nhận hàng: </span>
+                            <span>
+                                {
+                                    orderDetails.method === 'Paypal' ? ` ${orderDetails.address.address_line_1}, ${orderDetails.address.admin_area_2}` :
+                                        ` ${(orderDetails.address.detailAddress || '')} ${orderDetails.address.ward.label}, ${orderDetails.address.district.label}, ${orderDetails.address.city.label}`
+                                }
+                            </span>
+                        </div>
+                    </div>
+                    <div className='billing__order_infor box-style_infor col l-3 m-6 c-12'>
+                        <span className='heading__box'>Thông tin thanh toán</span>
+                        <div className='box-detail_infor'>
+                            <span>
+                                {
+                                    orderDetails.method === 'Online' ? 'Thẻ tín dụng' : 'Thanh toán khi nhận hàng'
+                                }
+                            </span>
+                            
+                            <div className='payment__detail_'>
+                                <img src={
+                                    orderDetails.method === 'Online' ? VisaLogo : CodLogo
+                                }/>
+                                <span>
+                                {
+                                    orderDetails.method === 'Online' && orderDetails.isPaid === true ? 
+                                    `Đã thanh toán: ${new Date(orderDetails.createdAt).toLocaleDateString() + ' ' + moment(orderDetails.createdAt).format('LT')}` :
+                                    orderDetails.method === 'COD' && orderDetails.isPaid === true ? 
+                                    `Đã thanh toán: ${new Date(orderDetails.updatedAt).toLocaleDateString() + ' ' + moment(orderDetails.updatedAt).format('LT')}` :
+                                    'Chưa thanh toán'
+                                }
+                            </span>
                             </div>
                         </div>
+                    </div>
+                    <div className='status__order_infor box-style_infor col l-3 m-6 c-12'>
+                        <span className='heading__box'>Trạng thái đơn hàng</span>
+                        <div className='box-detail_infor'>
+                            {
+                                orderDetails.status === 'Pending' ?
+
+                                <span style={{color: '#5e77bd', padding: '10px', fontSize: '14px', fontWeight: '500', border: '1px solid'}}>
+                                    Đang chờ xử lý
+                                </span> : orderDetails.status === 'Processing'  ?  
+                                <span style={{color: '#5e77bd', padding: '10px', fontSize: '14px', fontWeight: '500', border: '1px solid'}}>
+                                    Đang xử lý
+                                </span>  :  orderDetails.status === 'Shipping'  ?  
+                                <span style={{color: '#d16704', padding: '10px', fontSize: '14px', fontWeight: '500', border: '1px solid'}}>
+                                    Đang giao hàng
+                                </span>  :  orderDetails.status === 'Delivered' ?
+                                <span style={{color: '#0d9b25', padding: '10px', fontSize: '14px', fontWeight: '500', border: '1px solid'}}>
+                                    Đã giao hàng
+                                </span> : 
+                                <span style={{color: '#d93131', padding: '10px', fontSize: '14px', fontWeight: '500', border: '1px solid'}}>
+                                    Đã hủy
+                                </span>
+
+                            }                       
+                        </div>
+                        <div className='bottom__box'>
                         {
                             orderDetails.status === 'Delivered' ? 
-                            <div className='complete-order-noti'>
-                                <button disabled>Đã hoàn thành</button>
-                            </div> :
+                            null :
                             orderDetails.status === 'Processing' || orderDetails.status === 'Shipping' || orderDetails.status === 'Cancel' ?                    
                             <div className='cancel-order-disabled'>
-                                <button onClick={handleCancelOrder} className='disabled-btn' disabled>Hủy đơn hàng</button>
+                                <button onClick={handleCancelOrder} className='disabled-btn' disabled><IoTrashBin /> Hủy đơn hàng</button>
                             </div> :
                             <div className='cancel-order'>
-                                <button onClick={handleCancelOrder} >Hủy đơn hàng</button>
+                                <button onClick={handleCancelOrder} ><IoTrashBin /> Hủy đơn hàng</button>
                             </div> 
                           
-                        }
-                        
+                        } 
+                        </div>
                     </div>
                 </div>
             </div>
